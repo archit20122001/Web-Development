a8703123b93ef6aaddeea0e5f20e53c0fe65b943
archit20122001 <archit20122001@gmail.com>
2026-02-10 14:37:48 +0530

    Strings

---
+++ string.js
@@ -0,0 +1,37 @@
+// let firstName1 = "Archit Kumar";
+// let lastName = 'Singh';
+// console.log(firstName1);
+// console.log(lastName);
+
+// let name = `Archit
+// Kumar
+// Singh`
+// console.log(name);
+
+// let firstName2 = new String("Archit Kumar");
+// console.log(firstName2);
+
+// Operations on string:
+
+// Concatination--->
+let op1 = 'English ';
+let op2 = 'Physics ';
+let op3 = 'chemistry ';
+let op4 = 'GEOLOGY ';
+let ans = op1 + op2;
+console.log(ans);
+
+// using backticks--->
+let finalAnswer = `${op1}${op2}`;
+console.log(finalAnswer);
+
+console.log(op2.length);
+console.log(op3.toUpperCase());
+console.log(op4.toLowerCase());
+console.log(op4.substring(3));
+console.log(op4.substring(3,6));
+
+let sentence = "Hello everyone, \"how\" are you?";
+console.log(sentence);
+let words = sentence.split(" ");
+console.log(words);
