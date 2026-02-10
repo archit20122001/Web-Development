93dc1c98d7a380d98cd27ce23214913cff36e935
archit20122001 <archit20122001@gmail.com>
2026-02-10 11:49:46 +0530

    looping

---
+++ loop.js
@@ -0,0 +1,57 @@
+// for (let i = 1; i <= 10; i++) {
+//     console.log("Archit");
+// }
+
+// for (let i = 1; i <= 10; i++) {
+//     console.log(i);
+// }
+
+// for (let i = 5; i > 0; i--) {
+//     console.log(i);
+// }
+
+// for (let i = 1; i <= 6; i++) {
+//     if (i == 4)
+//         break;
+//     else
+//         console.log(i);
+// }
+
+// for (let i = 1; i <= 6; i++) {
+//     if (i == 3)
+//         continue;
+//     else
+//         console.log(i);
+// }
+
+// let i = 1;
+let i = 10;
+
+// while (i <= 5) {
+//     console.log(i);
+//     i++;
+// }
+
+// while (i < 6) {
+//     if (i == 3){
+//         i++;
+//         continue;
+//     }
+//     else
+//         console.log(i);
+//     i++;
+// }
+
+// do {
+//     console.log(i);
+//     i++;
+// }
+// while (i < 10);
+
+do {
+    console.log(i);
+    i--;
+}
+while (i > 4);
+
+// The problem with do while loop is it will execute the first iteration without checking the condition, so it will run once no matter what.
