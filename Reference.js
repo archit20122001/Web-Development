41714432f292ee02483f1d9a958a089eb02e3821
archit20122001 <archit20122001@gmail.com>
2026-02-11 15:09:21 +0530

    Referencing

---
+++ Reference.js
@@ -0,0 +1,12 @@
+let obj = {
+    name: "Archit",
+    age: 25,
+    weight: 85,
+    height: "5'9 feet",
+    greet: function(){
+        console.log('hello');
+    }
+};
+
+console.log(obj);
+obj.greet();
