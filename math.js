5be8c182a308ad569f3354762ac7a25568349dd8
archit20122001 <archit20122001@gmail.com>
2026-02-12 17:21:27 +0530

    Maths functions

---
+++ math.js
@@ -0,0 +1,15 @@
+console.log("Archit");
+
+console.log(Math.PI);
+console.log(Math.max(60, 48, 245, 147));
+console.log(Math.min(60, 48, 245, 147));
+console.log(Math.round(1.4));
+console.log(Math.floor(1.4));
+console.log(Math.ceil(1.4));
+console.log(Math.abs(-21));
+console.log(Math.random());
+console.log(Math.sqrt(4));
+console.log(Math.pow(2,4));
+console.log(Math.sin(90));
+console.log(Math.cos(90));
+console.log(Math.tan(90));
