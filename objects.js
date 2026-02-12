1118b169e045b4249773b99b9cb1cbc6869cc7ae
archit20122001 <archit20122001@gmail.com>
2026-02-12 18:00:21 +0530

    Date and other objects

---
+++ objects.js
@@ -0,0 +1,7 @@
+let curr = new Date();
+console.log(curr);
+let date = new Date('December 20 2025 10:52');
+let newDate = new Date( 2000, 11, 20, 7)
+console.log(newDate);
+newDate.getDay();
+console.log(newDate.getDay());
