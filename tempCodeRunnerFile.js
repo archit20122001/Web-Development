71dd179c685c402a5fd73b26052eacc4db1e5f02
archit20122001 <archit20122001@gmail.com>
2026-02-15 13:25:04 +0530

    Error handling

---
+++ tempCodeRunnerFile.js
@@ -0,0 +1,8 @@
+ry{
+//     // Reference error
+//     console.log(a);
+// }
+// catch(err){
+//     // Custom error message
+//     throw new Error("First declare the value of the variable");
+// }
