1d9e58f71c7b9895c76f683ab53c289d9f8fcac3
archit20122001 <archit20122001@gmail.com>
2026-03-13 14:42:59 +0530

    React Redux learning project

---
+++ React/RTK-Todo/src/App.jsx
@@ -0,0 +1,15 @@
+import AddTodo from "./components/AddTodo"
+import Todos from "./components/Todos"
+import React from 'react'
+
+function App() {
+
+  return (
+    <>
+      <AddTodo />
+      <Todos />
+    </>
+  )
+}
+
+export default App
