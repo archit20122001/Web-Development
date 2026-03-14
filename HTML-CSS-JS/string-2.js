0cf5790ac69447b8709f044668a7b5b3d2c2dd9f
archit20122001 <archit20122001@gmail.com>
2026-03-14 23:24:34 +0530

    Basic revisions today, nothing new

---
+++ HTML-CSS-JS/string-2.js
@@ -0,0 +1,13 @@
+const name = "Archit";
+const repoCount = 2;
+console.log(name + repoCount + "Value");
+console.log(`Hello Everyone, my name is ${name} and my repo count is ${repoCount}`);
+
+const gameName = new String ('silent hunter');
+console.log(gameName);
+console.log(gameName.length);
+console.log(gameName.toUpperCase());
+console.log(gameName.charAt(2));
+console.log(gameName.indexOf('h'));
+console.log(gameName.lastIndexOf('n'));
+console.log(gameName.slice(1,4));
