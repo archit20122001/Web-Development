22c30ba7651125579770a2ab021945fe12755a4b
archit20122001 <archit20122001@gmail.com>
2026-03-23 12:42:30 +0530

    RTE and Postform components added

--- React/Bloggist/src/Components/index.js
+++ React/Bloggist/src/Components/index.js
@@ -1,7 +1,11 @@
 import Header from './Header/Header'
 import Footer from './Footer'
 import Container from './Container';
-import LogoutBtn from './Header/LogoutBtn';
+import LogoutBtn from './Header/LogoutBtn'
 import Logo from './Logo'
+import RTE from './RTE'
+import Button from './Button'
+import Input from './Input'
+import Select from './Select'
 
-export { Header, Footer, Container, LogoutBtn, Logo };
+export { Header, Footer, Container, LogoutBtn, Logo, RTE, Button, Input, Select };
