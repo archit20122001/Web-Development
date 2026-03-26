76ecdb3fd516f02038283a8f6c6c25cfa1cb2dc4
archit20122001 <archit20122001@gmail.com>
2026-03-27 01:39:16 +0530

    Site deployed

--- React/Bloggist/src/main.jsx
+++ React/Bloggist/src/main.jsx
@@ -5,7 +5,7 @@ import './App.css'
 import App from './App.jsx'
 import { Provider } from 'react-redux'
 import store from './Store/store'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import { createHashRouter, RouterProvider } from 'react-router-dom'
 import { AuthLayout, Login } from './Components/index.js'
 import Home from './Pages/Home.jsx'
 import LogIn from './Pages/LogIn.jsx'
@@ -15,7 +15,7 @@ import AddPost from './Pages/AddPost.jsx'
 import EditPost from './Pages/EditPost.jsx'
 import Post from './Pages/Post.jsx'
 
-const router = createBrowserRouter([
+const router = createHashRouter([
   {
     path: "/",
     element: <App />,
