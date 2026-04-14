d5fa9d64b018b45fc2f87bb0b5a1b65afe2f6ad2
archit20122001 <archit20122001@gmail.com>
2026-04-14 23:18:59 +0530

    JavaScript Logic Day 4

---
+++ JavaScript-Logics/4/script.js
@@ -0,0 +1,10 @@
+const age = 20;
+if(age>=18){
+    console.log("Yes, You can vote");
+}
+else {
+    console.log("Nope, cannot vote yet");
+}
+
+const age2 = 20;
+age2 >=18 ? console.log("Yes, you are allowed") : console.log("No, You are not allowed")
